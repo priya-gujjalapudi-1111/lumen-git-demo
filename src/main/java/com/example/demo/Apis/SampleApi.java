da7e06d5740dd77a25c7d2bf78eac75ed9b59816
Lakshmi Priya Gujjalapudi <Gujjalapudi.LakshmiPriya@Lumen.com>
2024-01-10 12:54:08 +0530

    Api added

---
+++ src/main/java/com/example/demo/Apis/SampleApi.java
@@ -0,0 +1,16 @@
+package com.example.demo.Apis;
+
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/sample")
+public class SampleApi {
+    @GetMapping
+    public ResponseEntity<String> get(){
+        return ResponseEntity.ok("Hello World");
+    }
+}
